3f5a00c3f0f29825e56b1bbeedd91a7b03ef41db
shoreya <gupta.shoreya2707@gmail.com>
2021-05-09 19:19:16 +0530

    Dice Roll App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    //IBOutlet allows me to refer UIElements in IB
+    @IBOutlet weak var diceImageViewone: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //let keyword is used to define
+        //a constant value which in future
+        //is not gonna change
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),
+                         #imageLiteral(resourceName: "DiceTwo"),
+                         #imageLiteral(resourceName: "DiceThree"),
+                         #imageLiteral(resourceName: "DiceFour"),
+                         #imageLiteral(resourceName: "DiceFive"),
+                         #imageLiteral(resourceName: "DiceSix")]
+        
+        /*
+         Randomisation in swift is achieved by multiple way
+         1 --> Int.random(0...5) here ... represents range
+         2 --> array.randomelement() ---> method of an array
+         */
+        diceImageViewone.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        //print(diceImageView2.image)
     }
-
-
 }
-
